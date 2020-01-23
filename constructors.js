5cef482aaa7e5be03c3efb70e268ecf83136b4c3
Jessica Myer <36251741+oeddiemeddie@users.noreply.github.com>
2020-01-22 17:05:49 -0700

    commit the project

--- constructors.js
+++ constructors.js
@@ -16,6 +16,11 @@
 
 // Code here
 
+function CarFactory(make, model) {
+  this.make = make
+  this.model = model
+}
+
 ////////// PROBLEM 2 //////////
 
 // Do not edit the code below.
@@ -34,6 +39,11 @@ function Employee(name, email, hireDate) {
   */
   
   // Code here
+
+  var bob = new Employee('Bob', 'bob@gmail.com', '01-02-98') 
+
+  
+
   
   ////////// PROBLEM 3 //////////
   
@@ -43,6 +53,8 @@ function Employee(name, email, hireDate) {
   var mustang = new Car('Ford', 'Mustang', 2013);
   prius.moveCar(); // Increments prius' move property by 10. Returns the new move property.
   mustang.moveCar(); // Increments mustang' move property by 10. Returns the new move property.
+
+  
   
   // Do not edit the code above.
   
@@ -58,6 +70,32 @@ function Employee(name, email, hireDate) {
   
   // Code here
   
+function Car(make, model, year){
+  this.make = make
+  this.model = model
+  this.year = year
+  this.move = 0
+  this.moveCar = function() {
+    this.move += 10 
+    return this.move
+  }
+}
+
+// Could also be: 
+// function Car(make, model, year){
+//   this.make = make
+//   this.model = model
+//   this.year = year
+//   this.move = 0
+//   this.moveCar = function() {
+//     return this.move += 10 
+//   }
+// }
+
+
+
+
+
   ////////// PROBLEM 4 //////////
   
   /*
@@ -76,6 +114,18 @@ function Employee(name, email, hireDate) {
   }
   
   // Code here
+
+  Movie.prototype.changeRating = function(newRating) {
+    var average = (this.rating + newRating) / 2
+    this.rating = average
+    return this.rating
+  }
+
+  // Could also be: 
+  // Movie.prototype.changeRating = function(newRating) {
+  //   var average = (this.rating + newRating) / 2
+  //   return this.rating = average
+  // }
   
   ////////// PROBLEM 5 //////////
   
@@ -84,6 +134,22 @@ function Employee(name, email, hireDate) {
   // Once the User constructor function is created, write a prototype method for the User function. Name this method addSavedPost. It should take in three parameters: id (a number), title (a string) and rating (a number). Use these parameters to create a new object and add it to the savedPosts array. Make sure to name the properties the same as described previously (id, title, rating).
   
   // Code here
+
+  function User(name, age, email, savedPosts) {
+    this.name = name
+    this.age = age
+    this.email = email
+    this.savedPosts = savedPosts
+  }
+  
+  User.prototype.addSavedPost = function(id, title, rating) {
+    let newPost = {
+      id,
+      title,
+      rating
+    }
+    this.savedPosts.push(newPost)
+  }
   
   ////////// PROBLEM 6 //////////
   
@@ -91,6 +157,11 @@ function Employee(name, email, hireDate) {
   // Write a prototype method for the User constructor function named removeSavedPost that will take in one number parameter representing the post id. Use this id to find and remove the matching object in the savedPosts array.
   
   // Code here
+  User.prototype.removeSavedPost = function(id) {
+    this.savedPosts = this.savedPosts.filter(element => {
+      return element.id !== id
+    })
+  } //////////////////ASK ABOUT THIS IN REVIEW
   
   ////////// PROBLEM 7 //////////
   
@@ -98,4 +169,20 @@ function Employee(name, email, hireDate) {
   // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
   
   // Code here
+  User.prototype.changePostRating = function (id, newRating) {
+    this.savedPosts.forEach(function(post){
+      if(post.id === id){
+        post.rating = newRating
+      }
+  })
+  }
+
+  // Could also be:
+  // User.prototype.changePostRating = function (id, newRating) {
+  //   this.savedPosts.forEach(post => {
+  //     if(post.id === id){
+  //       post.rating = newRating
+  //     }
+  // })
+  // }
   
